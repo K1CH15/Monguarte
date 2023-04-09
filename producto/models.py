d24104d9fe2286741f4942701d44577fe10ab738
Cristian Camilo Perez Lopez <ccperez@soy.sena.edu.co>
2023-04-09 18:34:18 -0500

    Modelos V1.1

--- producto/models.py
+++ producto/models.py
@@ -1,22 +1,35 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 #Modelo del módulo Productos
 # Create your models here.
 class Producto(models.Model):
     #Modelo de producto
     id_producto=models.AutoField(primary_key=True)
-    nombre_producto=models.CharField(max_length=45,verbose_name="Nombre del producto")
-    precio_unidad_producto=models.FloatField(max_length=10,verbose_name="Precio unitario")
-    cantidad_producto=models.IntegerField(max_length=10,verbose_name="Cantidad de productos")
-    id_tipo_producto=models.ForeignKey("producto.Tipo_Producto")
-    id_tamaño_producto=models.ForeignKey("producto.Tamaño_Producto")
-    id_stock_producto=models.ForeignKey("stock.Stock")
+    nombre=models.CharField(max_length=45,verbose_name="Nombre")
+    precio_unidad=models.FloatField(max_length=10,verbose_name="Precio unitario")
+    cantidad=models.IntegerField(max_length=10,verbose_name="Cantidad")
+    #id_tipo_producto=models.ForeignKey()
+    #id_tamaño_producto=models.ForeignKey()
+    #id_stock_producto=models.ForeignKey(")
+    class Estado(models.TextChoices):
+        ACTIVO='1',_("Activo")
+        INACTICO='0',_("Inactivo")
+    estado=models.CharField(max_length=1,choices=Estado.choices,default=Estado.ACTIVO,verbose_name="Estado")
 
 #Modelo de Tipo de Producto
 class Tipo_Producto(models.Model):
-    #llave
-    id_tipo_producto=models.AutoField(primary_key=True)
-    nombre_tipo_producto=models.CharField(max_length=45,verbose_name="Tipo de Producto")
+    
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=30,verbose_name="Tipo de Producto")
+    class Estado(models.TextChoices):
+        ACTIVO='1',_("Activo")
+        INACTICO='0',_("Inactivo")
+    estado=models.CharField(max_length=1,choices=Estado.choices,default=Estado.ACTIVO,verbose_name="Estado")
 #Modelo de Tamaño Producto
 class Tamaño_Producto(models.Model):
-    id_tamaño_producto=models.AutoField(primary_key=True)
-    nombre_tamaño_producto=models.CharField(max_length=45,verbose_name="Tamaño")
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=30,verbose_name="Tamaño")
+    class Estado(models.TextChoices):
+        ACTIVO='1',_("Activo")
+        INACTICO='0',_("Inactivo")
+    estado=models.CharField(max_length=1,choices=Estado.choices,default=Estado.ACTIVO,verbose_name="Estado")
--- venta/models.py
+++ venta/models.py
@@ -1,31 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import integer_validator,MaxLengthValidator
 #Módulo de venta
 # Create your models here.
-#Modelo de Cliente
-class Cliente(models.Models):
-    registro=models.IntegerField(max_length=25,verbose_name="Numero de registro")
-    class TipoDocumento(models.TextChoices):
-        CC='1',("Cédula de Ciudadanía")
-        TI='2',("Tarjeta de Identidad")
-        CE='3',("Cédula de Extranjería")
-    tipo_documento=models.CharField(max_length=1,choices=TipoDocumento.choices,default=TipoDocumento.CC,verbose_name="Tipo de Documento")
-    numero_documento_cliente=models.IntegerField(primary_key=True,max_length=20,verbose_name="Numero de documento del cliente")
-    nombre=models.CharField(max_length=45,verbose_name="Nombre")
-    apellido=models.CharField(max_length=45,verbose_name="Apellido")
-    numero_telefono=models.IntegerField(max_length=10,verbose_name="Numero Telefónico")
-    direccion=models.CharField(max_length=50,verbose_name="Dirección")
+
 #Modelo de Venta
 class Venta(models.Model):
-    id_venta=models.AutoField(primary_key=True,)
-    fecha=models.DateTimeField(verbose_name="Fecha")
+    id=models.AutoField(primary_key=True)
+    fecha=models.DateTimeField(verbose_name="Fecha",auto_now_add=True)
     valor_total=models.DecimalField(max_length=10,decimal_places=2,verbose_name="Valor total")
-    numero_documento_cliente=models.ForeignKey("venta.Cliente",verbose_name="Número Documento Cliente")
-    numero_documento_persona=models.ForeignKey("persona.Persona",verbose_name="Número de Documento Persona")
+    class Estado(models.TextChoices):
+        ACTIVO='1',_("Activo")
+        INACTICO='0',_("Inactivo")
+    estado=models.CharField(max_length=1,choices=Estado.choices,default=Estado.ACTIVO,verbose_name="Estado")
+
+
+    #numero_documento_persona=models.ForeignKey()
+
 #Modelo de Detalle Venta
 class Detalle_Venta(models.Models):
-    id_detale_venta=models.AutoField(primary_key=True)
-    descrpicion=models.TextField(max_length=100,verbose_name="Descripción")
-    cantidad_total=models.IntegerField(max_length=20,verbose_name="Cantidad Total")
-    precio_unitario=models.FloatField(max_length=10,verbose_name="Precio Unitario")
-    id_venta=models.ForeignKey()
-    id_stock_producto=models.ForeignKey()
+    id=models.AutoField(primary_key=True)
+    precio_unitario=models.DecimalField(max_digits=25, decimal_places=5, verbose_name="Precio Unitario")
+    cantidad_total = models.IntegerField(verbose_name="Cantidad Total")
+    #id_venta=models.ForeignKey()
+    #id_materia_prima=models.ForeignKey()
