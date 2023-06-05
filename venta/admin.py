f90dd720cbc226717e7cfc63cc23928a62973bff
K1CH15 <gumball1618@gmail.com>
2023-06-05 16:47:10 -0500

    body con icono

--- venta/admin.py
+++ venta/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from venta.models import Venta, Detalle_Venta
 # Register your models here.
+
+admin.site.register(Venta)
+admin.site.register(Detalle_Venta)
+#admin.site.register(Detalle_Venta_Venta)
