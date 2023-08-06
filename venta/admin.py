aaba05773358b4f0d236cb6debde9c936dd97852
Jonathan David Montañez Pérez <jdmontanez47@soy.sena.edu.co>
2023-08-05 20:46:36 -0500

    Eliminando todos los archivos

--- venta/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from venta.models import Venta, Detalle_Venta
-# Register your models here.
-
-admin.site.register(Venta)
-admin.site.register(Detalle_Venta)
-#admin.site.register(Detalle_Venta_Venta)
